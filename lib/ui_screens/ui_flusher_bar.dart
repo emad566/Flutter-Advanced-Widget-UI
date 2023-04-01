9473561abf6b795603351a221c9e25ae531fff1f
emad566 <emade09@gmail.com>
2023-04-01 15:51:05 +0200

    FlushBar and drawer serch

--- lib/ui_screens/ui_flusher_bar.dart
+++ lib/ui_screens/ui_flusher_bar.dart
@@ -1,4 +1,6 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_ui/shared/components.dart';
 
 class UiFlushbar extends StatefulWidget {
   const UiFlushbar({Key? key}) : super(key: key);
@@ -16,13 +18,39 @@ class _UiFlushbarState extends State<UiFlushbar> {
           child: Container(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: const [
-                Text('UiFlushbar'),
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    showMyFlushBar(context);
+                  },
+                  child: myText('Show Flush Bar'),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void showMyFlushBar(BuildContext context) {
+    Flushbar(
+      flushbarPosition: FlushbarPosition.TOP,
+      mainButton: TextButton(
+        onPressed: (){Navigator.pop(context);},
+        child: const Text('Close!', style: TextStyle(fontSize: 18, color: Colors.white),),
+      ),
+      backgroundColor: Colors.green,
+      title: "Title",
+      message:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      icon: Icon(
+        Icons.info_outline,
+        size: 28.0,
+        color: Colors.blue[300],
+      ),
+      duration: const Duration(seconds: 3),
+      leftBarIndicatorColor: Colors.blue[300],
+    ).show(context);
   }
 }
