fd2043557a23edd41c8e83037161bf548f04caba
emad566 <emade09@gmail.com>
2023-04-02 14:49:19 +0200

    Switch

--- lib/ui_screens/ui_switch.dart
+++ lib/ui_screens/ui_switch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_ui/shared/components.dart';
 
 class UiSwitch extends StatefulWidget {
   const UiSwitch({Key? key}) : super(key: key);
@@ -8,16 +9,27 @@ class UiSwitch extends StatefulWidget {
 }
 
 class _UiSwitchState extends State<UiSwitch> {
+  bool isBgLight = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
+            color: isBgLight ? Colors.white : Colors.green,
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: const [
-                Text('UiSwitch'),
+              children: [
+                myText('BgMode'),
+                Switch(
+                  value: isBgLight,
+                  onChanged: (val){
+                    setState(() {
+                      isBgLight = val;
+                    });
+                  },
+                ),
               ],
             ),
           ),
