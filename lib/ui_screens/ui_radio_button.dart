d5e4cd5651685fa328b97b2acc46f5917f310fcb
emad566 <emade09@gmail.com>
2023-04-02 10:25:08 +0200

    RadionButton

--- lib/ui_screens/ui_radio_button.dart
+++ lib/ui_screens/ui_radio_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_ui/shared/components.dart';
 
 class UiRadioButton extends StatefulWidget {
   const UiRadioButton({Key? key}) : super(key: key);
@@ -8,6 +9,32 @@ class UiRadioButton extends StatefulWidget {
 }
 
 class _UiRadioButtonState extends State<UiRadioButton> {
+  dynamic groupValue;
+
+  void myDialog(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      title: myText('Answer'),
+      content: Container(
+        color: groupValue == 'Emad' ? Colors.white : Colors.red,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            myText(groupValue == 'Emad' ? 'True' : 'False'),
+            ElevatedButton(
+              onPressed: () {Navigator.pop(context);},
+              child: myText('ok'),
+            ),
+          ],
+        ),
+      ),
+    );
+    showDialog(context: context, builder: (_) => alert);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +43,48 @@ class _UiRadioButtonState extends State<UiRadioButton> {
           child: Container(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: const [
-                Text('UiRadioButton'),
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                myText('What is your name=?'),
+                buildRadioButton('Emad', context),
+                buildRadioButton('Mohamed', context),
+                buildRadioButton('Ali', context),
+                const Divider(
+                  color: Colors.black,
+                  thickness: 2,
+                ),
+                myText('You Selected'),
+                myText('$groupValue'),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildRadioButton(dynamic value, BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          groupValue = value;
+          myDialog(context);
+        });
+      },
+      child: Row(
+        children: [
+          Radio(
+            value: value,
+            groupValue: groupValue,
+            onChanged: (val) {
+              setState(() {
+                groupValue = val;
+                myDialog(context);
+              });
+            },
+          ),
+          myText(value),
+        ],
       ),
     );
   }
